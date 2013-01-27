349bb728ec394f3d9f6a9b350914557cc3b4053c
Mohammed Majeed <mmajeed668@gmail.com>
2013-01-27 15:33:37 -0500

    Created a new cs file that will hold all the logic for saving

---
+++ TextEditor/TextEditor/XMLLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Class that will hold all the logic for saving files
+    /// </summary>
+    public static class XMLLogic
+    {
+
+    }
+}
